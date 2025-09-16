fb57f6692c74e3331a39f42605979112f643a9cc
Pedro Augusto <pedroaeickhoff@gmail.com>
2025-09-16 13:38:48 -0300

    Inicio

---
+++ src/main/java/IBGE/demo/IbgeApplication.java
@@ -0,0 +1,13 @@
+package IBGE.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbgeApplication.class, args);
+	}
+
+}
